9a76b8aab4d0f11e83b3e6f4e7be6e79e29c9b3c
wukeqi <1270314454qq.com>
2024-08-15 14:41:03 +0800

    menu

--- .vitepress/config/nav.js
+++ .vitepress/config/nav.js
@@ -1,27 +1,6 @@
 export const nav = [
   { text: 'Home', link: '/' },
   {
-    text: '程序员客栈',
-    activeMatch: '^/programmer',
-    items: [
-      { text: '计算机基础', link: '/programmer/basic' },
-      { text: '前端', link: '/programmer/front' },
-      { text: '后端', link: '/programmer/back' },
-      { text: '运维', link: '/programmer/ops' },
-      { text: '网络', link: '/programmer/network' },
-    ],
-  },
-  {
-    text: '工具分享',
-    activeMatch: '^/tool',
-    items: [
-      { text: '在线工具', link: '/tool/online' },
-      { text: '软件下载', link: '/tool/software' },
-      { text: '实用网站', link: '/tool/website' },
-      { text: 'MacOS', link: '/tool/mac' },
-    ],
-  },
-  {
     text: '知识库',
     activeMatch: '^/knowledge',
     items: [
@@ -40,11 +19,32 @@ export const nav = [
     ],
   },
   {
+    text: '程序员客栈',
+    activeMatch: '^/programmer',
+    items: [
+      { text: '计算机基础', link: '/programmer/basic' },
+      { text: '前端', link: '/programmer/front' },
+      { text: '后端', link: '/programmer/back' },
+      { text: '运维', link: '/programmer/ops' },
+      { text: '网络', link: '/programmer/network' },
+    ],
+  },
+  {
     text: '生活日记',
     activeMatch: '^/life',
     items: [
       { text: '旅游攻略', link: '/life/tourism' },
       { text: '美食聚餐', link: '/life/delicacy' },
+    ],
+  },
+  {
+    text: '工具分享',
+    activeMatch: '^/tool',
+    items: [
+      { text: '在线工具', link: '/tool/online' },
+      { text: '软件下载', link: '/tool/software' },
+      { text: '实用网站', link: '/tool/website' },
+      { text: 'MacOS', link: '/tool/mac' },
     ],
   },
 ]
